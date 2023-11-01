f111a2a5c71aa6fddc49ff76fc6d8f394422b3d2
quantumdot <quantumdot@users.noreply.github.com>
2023-11-01 17:59:47 -0400

    fix event stat computation

--- med_associates_utils/proc.py
+++ med_associates_utils/proc.py
@@ -1,6 +1,8 @@
 from typing import Literal, Union
 import pandas as pd
 
+from med_associates_utils.session import FieldList, SessionCollection
+
 
 
 def calculate_cumulative_events(df: pd.DataFrame, event: Union[str, list[str]], groupby: Union[str, list[str]], time_key: str = 'time') -> pd.DataFrame:
@@ -11,44 +13,57 @@ def calculate_cumulative_events(df: pd.DataFrame, event: Union[str, list[str]], 
     return df
 
 
-def calculate_event_series_stats(df: pd.DataFrame, event: Union[str, list[str]], groupby: Union[str, list[str]], max_events: int, max_time: int, time_key: str = 'time', event_key: str = 'event', form: Literal['long', 'wide'] = 'wide') -> pd.DataFrame:
+def calculate_event_series_stats(sessions: SessionCollection, event: Union[str, list[str]], max_events: int, max_time: int, include_meta: FieldList = 'all',
+                                 time_key: str = 'time', event_key: str = 'event', form: Literal['long', 'wide'] = 'wide') -> pd.DataFrame:
     ''' Calculate statistics on event series
         The following are calculated:
             - latency: amount of time elapsed since the start of the trial until the first event is emitted
             - last: amount of time elapsed since the start of the trial unitl the last event is emitted
-            - finished: amount of time elapsed since the start of the trial unitl all rewards are earned (equivalent to `last`) or the trial has completed (equivalent to `max_time`)
+            - finished: amount of time elapsed since the start of the trial until all rewards are earned (equivalent to `last`) or the trial has completed (equivalent to `max_time`)
             - count: number of events emitted in the series
             - completed: boolean indicating if the number of emitted events is greater than or equal to `max_events`
     '''
     if isinstance(event, str):
         event = [event]
 
-    metrics = {
-        'latency': (time_key, 'min'),
-        'last': (time_key, 'max'),
-        'finished': (time_key, 'max'),
-        'count': (time_key, 'count'),
-    }
+    summaries = []
+    for session in sessions:
+        if include_meta == 'all':
+            meta = session.metadata
+        else:
+            meta = {k: v for k, v in session.metadata.items() if k in include_meta}
 
-    dfs = []
-    for evt in event:
-        subset = df[df[event_key] == evt]
-        stats = subset.groupby(by=groupby).aggregate(**metrics).reset_index()
-        dfs.append(stats)
-
-    complete = pd.concat(dfs).reset_index(drop=True)
-
-    complete['completed'] = complete['count'] >= max_events
-    complete.loc[~complete['completed'], 'finished'] = max_time
+        for e in event:
+            df = session.to_dataframe(include_arrays=[e], include_meta=[])
+            if df.empty:
+                summaries.append({
+                    **meta,
+                    'event': e,
+                    'latency': max_time,  # no events were emitted, so latency will be the trial duration
+                    'last': max_time,  # no events were emitted, so last will be the trial duration
+                    'finished': max_time,
+                    'count': 0,
+                    'completed': False,
+                })
+            else:
+                count = len(df.index)
+                last = df.time.max()
+                summaries.append({
+                    **meta,
+                    'event': e,
+                    'latency': df.time.min(),  # time of the first event
+                    'last': last,  # time of the last event
+                    'finished': max_time if count < max_events else last,  # if the trial was completed, use last event time, otherwise use the max trial duration
+                    'count': count,
+                    'completed': count >= max_events,
+                })
+    summaries = pd.DataFrame(summaries)
 
     if form == 'long':
         value_vars = ['latency', 'last', 'finished', 'count', 'completed']
-        id_vars = list(set(complete.columns.to_list()) - set(value_vars))
-        return complete.melt(id_vars=id_vars, value_vars=value_vars, var_name='metric', value_name='value', ignore_index=True)
+        id_vars = list(set(summaries.columns.to_list()) - set(value_vars))
+        return summaries.melt(id_vars=id_vars, value_vars=value_vars, var_name='metric', value_name='value', ignore_index=True)
     elif form == 'wide':
-        return complete
+        return summaries
     else:
         raise ValueError(f'Did not understand value provided for `form` parameter. Got "{form}"')
-
-
-    return complete
