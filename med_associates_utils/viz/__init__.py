2c48108aac14fe8920a55cf1dfd02248067e9087
quantumdot <quantumdot@users.noreply.github.com>
2024-09-27 18:14:07 -0400

    set matplotlib font types to truetype when importing viz module

--- med_associates_utils/viz/__init__.py
+++ med_associates_utils/viz/__init__.py
@@ -1,2 +1,7 @@
+import matplotlib as mpl
+
 from .cumulative import plot_cumulative_events
 from .raster import plot_event_raster
+
+# set some matplotlib parameters
+mpl.rcParams['pdf.fonttype'] = 42  # turn on true-type fonts, provides better editing of PDF/Illustrator
